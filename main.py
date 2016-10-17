2180aaaf0a6df176e74963abd2d0d32991e7f25a
Taylor Thurlow <taylorthurlow8@gmail.com>
2016-10-16 22:56:24 -0700

    Made logging better, to file.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 import time
-import os
+import os, sys
 import os.path as path
 import transmissionrpc
 import datetime
@@ -16,16 +16,26 @@ download_dir_tv = ''
 download_dir_movie = ''
 download_dir_music = ''
 
-delete = True # Currently you will receive errors in the log if you do not remove the torrent file - it'll be picked up the next
-              # time the script loops and will try and add the torrent again. Transmission will throw an exception because it's
-              # already added.
-
 client = transmissionrpc.Client(
-    address='',
-    port='',
+    address='127.0.0.1',
+    port='9091',
     user='',
     password=''
     )
+
+# Logging
+log = open('./log.txt', 'a')
+timestamp = '[{:%Y-%m-%d %H:%M:%S}]'.format(datetime.datetime.now())
+print >> log, timestamp +  ' ' + 'Started watch script.'
+print >> log, 'Current watch directories:'
+print >> log, '    TV: ' + watch_tv
+print >> log, 'Movies: ' + watch_movie
+print >> log, ' Music: ' + watch_music
+print >> log, 'Current download directories:'
+print >> log, '    TV: ' + download_dir_tv
+print >> log, 'Movies: ' + download_dir_movie
+print >> log, ' Music: ' + download_dir_music
+log.close()
 
 def add(watch_dir, download_dir):
     directory = os.listdir(watch_dir)
@@ -33,18 +43,25 @@ def add(watch_dir, download_dir):
     if files: # files exist in directory
         for file in files:
             if file.lower().endswith('.torrent') and not file.lower().startswith('.'):
+            log = open('./log.txt', 'a')
+            timestamp = '[{:%Y-%m-%d %H:%M:%S}]'.format(datetime.datetime.now())
+
                 try:
-                    print '[{:%Y-%m-%d %H:%M:%S}]'.format(datetime.datetime.now()), "Adding torrent:", file
+            print >> log, timestamp + ' ' + 'Adding torrent: ' + file
                     newTorrent = client.add_torrent(watch_dir + '/' + file, download_dir=download_dir)
                     time.sleep(1)
                     newTorrent.start()
-                    if delete: os.remove(watch_dir + '/' + file)
+                    os.remove(watch_dir + '/' + file)
                 except Exception, e:
-                    print '[{:%Y-%m-%d %H:%M:%S}]'.format(datetime.datetime.now()), "Error encountered:", str(e)
+            print >> log, timestamp + ' ' + 'Error encountered: ' + str(e)
+
+        log.close()
                 time.sleep(1)
 
 while True:
-    print '[{:%Y-%m-%d %H:%M:%S}]'.format(datetime.datetime.now()), "Searching directories."
+    log = open('./log.txt', 'a')
+    timestamp = '[{:%Y-%m-%d %H:%M:%S}]'.format(datetime.datetime.now())
+    print >> log, timestamp + ' ' + 'Searching directories.'
     add(watch_tv, download_dir_tv)
     add(watch_movie, download_dir_movie)
     add(watch_music, download_dir_music)
